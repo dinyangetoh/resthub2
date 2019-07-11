1c69656c4fe5b3deaac25af002d21824ef8e2622
David Inyangetoh <dinyangetoh@gmail.com>
2019-07-11 07:48:45 +0100

    initial commit with basic setup and packages

---
+++ api-routes.js
@@ -0,0 +1,12 @@
+// Filename: api-routes.js
+// Initialize express router
+let router = require('express').Router();
+// Set default API response
+router.get('/', function (req, res) {
+    res.json({
+        status: 'API Its Working',
+        message: 'Welcome to RESTHub crafted with love!'
+    });
+});
+// Export API routes
+module.exports = router;
---
+++ index.js
@@ -0,0 +1,32 @@
+// Import express
+let express = require('express');
+// Import Body parser
+let bodyParser = require('body-parser');
+// Import Mongoose
+let mongoose = require('mongoose');
+// Initialize the app
+let app = express();
+// Import routes
+let apiRoutes = require("./api-routes")
+// Configure bodyparser to handle post requests
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+// Connect to Mongoose and set connection variable
+mongoose.connect('mongodb://localhost/resthub');
+var db = mongoose.connection;
+
+
+// Setup server port
+var port = process.env.PORT || 8080;
+
+// Send message for default URL
+app.get('/', (req, res) => res.send('Hello World with Express'));
+
+// Use Api routes in the App
+app.use('/api', apiRoutes)
+// Launch app to listen to specified port
+app.listen(port, function () {
+    console.log("Running RestHub on port " + port);
+});

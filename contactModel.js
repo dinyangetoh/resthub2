f3b87cc5716d09b01fb194efe248a670f3576e6c
David Inyangetoh <dinyangetoh@gmail.com>
2019-07-11 07:52:52 +0100

    added contacts controller and model

---
+++ contactModel.js
@@ -0,0 +1,24 @@
+// contactModel.js
+var mongoose = require('mongoose');
+// Setup schema
+var contactSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: String,
+    phone: String,
+    create_date: {
+        type: Date,
+        default: Date.now
+    }
+});
+// Export Contact model
+var Contact = module.exports = mongoose.model('contact', contactSchema);
+module.exports.get = function (callback, limit) {
+    Contact.find(callback).limit(limit);
+}

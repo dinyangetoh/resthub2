6b821925a2f791fe802be4fd3cb1a448144eb239
David Inyangetoh <dinyangetoh@gmail.com>
2019-07-11 07:54:11 +0100

    updated API routes

--- api-routes.js
+++ api-routes.js
@@ -1,12 +1,24 @@
-// Filename: api-routes.js
+// api-routes.js
 // Initialize express router
 let router = require('express').Router();
 // Set default API response
 router.get('/', function (req, res) {
     res.json({
         status: 'API Its Working',
-        message: 'Welcome to RESTHub crafted with love!'
+        message: 'Welcome to RESTHub crafted with love!',
     });
 });
+// Import contact controller
+var contactController = require('./contactController');
+// Contact routes
+router.route('/contacts')
+    .get(contactController.index)
+    .post(contactController.new);
+
+router.route('/contacts/:contact_id')
+    .get(contactController.view)
+    .patch(contactController.update)
+    .put(contactController.update)
+    .delete(contactController.delete);
 // Export API routes
 module.exports = router;

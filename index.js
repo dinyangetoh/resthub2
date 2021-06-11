1c1bb04f2ddbe67f8a6a21959bcb0c7a27cc9110
sean890 <sean1360851@gmail.com>
2021-06-11 10:46:16 +1000

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 // Connect to Mongoose and set connection variable
-mongoose.connect('mongodb://localhost/freya-donation', { useNewUrlParser: true});
+mongoose.connect('mongodb://localhost/freya-donation1', { useNewUrlParser: true});
 
 // Heroku Mongoose connection
 // mongoose.connect('mongodb://heroku_5686p02g:sia8l3fni4jmu7qbn0ac1t75mf@ds349857.mlab.com:49857/heroku_5686p02g', { useNewUrlParser: true });
